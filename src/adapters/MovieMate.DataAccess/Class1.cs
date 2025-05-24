506c4021a2aa60b63b67883bc5e2b720b515c90f
Felipe Ferreira <felipe.w.ferreira@pwc.com>
2025-05-24 05:59:24 -0400

    chore: implements code styles

--- src/adapters/MovieMate.DataAccess/Class1.cs
+++ src/adapters/MovieMate.DataAccess/Class1.cs
@@ -1,6 +1,6 @@
 ﻿namespace MovieMate.DataAccess
 {
-	public class Class1
-	{
-	}
+    public class Class1
+    {
+    }
 }
--- src/shared/MovieMate.Application/Class1.cs
+++ src/shared/MovieMate.Application/Class1.cs
@@ -1,6 +1,6 @@
 ﻿namespace MovieMate.Application
 {
-	public class Class1
-	{
-	}
+    public class Class1
+    {
+    }
 }
