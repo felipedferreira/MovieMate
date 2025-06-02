5b2b51f7126654a92a86d44f98c67b2338365857
Felipe Ferreira <felipe.w.ferreira@pwc.com>
2025-06-02 15:00:39 -0400

    chore: adds very dumb test

--- src/presentation/MovieMate.API/Program.cs
+++ src/presentation/MovieMate.API/Program.cs
@@ -1,46 +1,65 @@
 using Scalar.AspNetCore;
 using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Configure Serilog
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .Enrich.FromLogContext()
-    .CreateLogger();
-
-builder.Host.UseSerilog();
-
-// Add services to the container.
-builder.Services.AddControllers();
-builder.Services.AddApplicationServices();
-
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-app.UseStaticFiles();
-
-//Add support to logging request with SERILOG
-app.UseSerilogRequestLogging();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace MovieMate.API
 {
-    // -> /openapi/v1.json
-    app.MapOpenApi();
-    // -> /scalar/v1
-    app.MapScalarApiReference(options =>
+    public class Program
     {
-        options.Theme = ScalarTheme.BluePlanet;
-        options.EnabledClients = [ScalarClient.HttpClient, ScalarClient.Axios, ScalarClient.Fetch];
-    });
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var builder = WebApplication.CreateBuilder(args);
+                // Configure Serilog
+                Log.Logger = new LoggerConfiguration()
+                    .WriteTo.Console()
+                    .Enrich.FromLogContext()
+                    .CreateLogger();
+                builder.Host.UseSerilog();
+
+                // Add services to the container.
+                builder.Services.AddControllers();
+                builder.Services.AddApplicationServices();
+
+                // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+                builder.Services.AddOpenApi();
+
+                var app = builder.Build();
+                app.UseStaticFiles();
+
+                //Add support to logging request with SERILOG
+                app.UseSerilogRequestLogging();
+
+                // Configure the HTTP request pipeline.
+                if (app.Environment.IsDevelopment())
+                {
+                    // -> /openapi/v1.json
+                    app.MapOpenApi();
+                    // -> /scalar/v1
+                    app.MapScalarApiReference(options =>
+                    {
+                        options.Theme = ScalarTheme.BluePlanet;
+                        options.EnabledClients = [ScalarClient.HttpClient, ScalarClient.Axios, ScalarClient.Fetch];
+                    });
+                }
+
+                app.UseHttpsRedirection();
+
+                app.UseAuthorization();
+
+                app.MapControllers();
+
+                await app.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                Log.Logger.Error(ex, "An error occurred while starting the application.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+    }
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ tests/integration/MovieMate.API.Integration/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using static MovieMate.API.ApiEndpoints;
+
+namespace MovieMate.API.Integration
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public async Task GetAllAsync()
+        {
+            using var factory = new WebApplicationFactory<Program>();
+            using var httpClient = factory.CreateDefaultClient();
+            var response = await httpClient.GetAsync(MovieApiEndpoints.GetAll);
+            response.EnsureSuccessStatusCode();
+        }
+    }
+}
